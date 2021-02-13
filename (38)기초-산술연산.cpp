c7278641963e5a1d05e585efd3f5ce9c7f48950e
woogoeun <78858362+woogoeun@users.noreply.github.com>
2021-02-13 23:45:06 +0900

    Add files via upload

---
+++ (38)기초-산술연산.cpp
@@ -0,0 +1,16 @@
+/* 38 */
+/*============================
+정수 2개 입력받아 합 출력하기1
+=============================*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	long long a, b;
+
+	cin >> a >> b;
+	cout << a + b;
+
+	return 0;
+}
