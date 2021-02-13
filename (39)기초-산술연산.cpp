99a83e59cc77f77ad9ee8aa8fbaf07d415639aab
woogoeun <78858362+woogoeun@users.noreply.github.com>
2021-02-13 23:55:44 +0900

    Add files via upload

---
+++ (39)기초-산술연산.cpp
@@ -0,0 +1,17 @@
+/* 39 */
+/*============================
+정수 2개 입력받아 합 출력하기2
+=============================*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	long long int a, b, sum;
+
+	cin >> a >> b;
+	sum = a + b;
+	cout << sum;
+
+	return 0;
+}
