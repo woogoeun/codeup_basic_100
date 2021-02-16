ead260d02ea958e852089c94dd85cdb9c777f409
woogoeun <78858362+woogoeun@users.noreply.github.com>
2021-02-16 21:01:58 +0900

    Add files via upload

---
+++ (59)기초-비트단위논리연산.cpp
@@ -0,0 +1,16 @@
+/* 59 */
+/*==========================
+비트단위로 NOT 하여 출력하기
+===========================*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a;
+
+	cin >> a;
+	cout << ~a;
+
+	return 0;
+}
---
+++ (60)기초-비트단위논리연산.cpp
@@ -0,0 +1,16 @@
+/* 60 */
+/*==========================
+비트단위로 AND 하여 출력하기
+===========================*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+
+	cin >> a >> b;
+	cout << (a & b);
+
+	return 0;
+}
